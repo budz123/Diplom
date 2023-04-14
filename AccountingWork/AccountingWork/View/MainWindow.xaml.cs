3f680be198075a58abf9f2a6605d58ddab15b8f2
BUDZ1974 <92312281+budz123@users.noreply.github.com>
2023-04-15 00:03:19 +0300

    +

--- AccountingWork/AccountingWork/View/MainWindow.xaml.cs
+++ AccountingWork/AccountingWork/View/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace AccountingWork
+namespace AccountingWork.View
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
